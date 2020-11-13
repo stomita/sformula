09bf749a42f49b3b0b6aeefb6faff08a18310aff
Shinichi Tomita <shinichi.tomita@gmail.com>
2020-11-13 10:37:23 +0900

    add test case for WEEKDAY

--- test/static.test.ts
+++ test/static.test.ts
@@ -31,3 +31,29 @@ test("should evaluate global reference (with $-prefixed variable)", () => {
   const ret1 = fml.evaluate({ $User: { Id: "u01" } });
   assert(ret1 === "u01");
 });
+
+test("should correctly calculate WEEKDAY()", () => {
+  const formula = "WEEKDAY(Date__c)";
+  const fml = parseSync(formula, {
+    inputTypes: {
+      Date__c: { type: "date" },
+    },
+    returnType: "number",
+  });
+  const ret0 = fml.evaluate({ Date__c: "2015-02-28" });
+  const ret1 = fml.evaluate({ Date__c: "2015-03-01" });
+  const ret2 = fml.evaluate({ Date__c: "2015-03-02" });
+  const ret3 = fml.evaluate({ Date__c: "2015-03-03" });
+  const ret4 = fml.evaluate({ Date__c: "2015-03-04" });
+  const ret5 = fml.evaluate({ Date__c: "2015-03-05" });
+  const ret6 = fml.evaluate({ Date__c: "2015-03-06" });
+  const ret7 = fml.evaluate({ Date__c: "2015-03-07" });
+  assert(ret0 === 7);
+  assert(ret1 === 1);
+  assert(ret2 === 2);
+  assert(ret3 === 3);
+  assert(ret4 === 4);
+  assert(ret5 === 5);
+  assert(ret6 === 6);
+  assert(ret7 === 7);
+});
