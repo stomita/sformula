ba7a8e576ce24225a6dc0371bc2f2313f844db4b
Shinichi Tomita <shinichi.tomita@gmail.com>
2025-04-24 09:12:12 +0900

    fix CASE value comparison is not made correctly for bignumber

--- src/builtin/types.ts
+++ src/builtin/types.ts
@@ -1,4 +1,4 @@
-import type { BigNumber } from "bignumber.js";
+import { BigNumber } from "bignumber.js";
 import type { Maybe } from "../types";
 
 export type SfString = Maybe<string>;
@@ -14,3 +14,20 @@ export type SfAnyData =
   | SfDate
   | SfDatetime
   | SfTime;
+
+function isNumber(v: SfAnyData): v is NonNullable<SfNumber> {
+  return BigNumber.isBigNumber(v) || typeof v === 'number';
+}
+
+export function isEquivalent(v1: SfAnyData, v2: SfAnyData): boolean {
+  if (v1 === v2) {
+    return true;
+  }
+  if ((v1 == null || v1 === '') && (v2 == null || v2 === '')) {
+    return true;
+  }
+  if (isNumber(v1) && isNumber(v2)) {
+    return BigNumber(v1).isEqualTo(v2);
+  }
+  return false;
+}
